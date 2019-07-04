83327eb4deb754cfdb31d32c96dbef96c3ee6437
Márcio Brufatto <mhbrufatto@gmail.com>
2019-07-04 18:53:03 -0300

    Initial Commit

---
+++ BeBlue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeBlue
+//
+//  Created by Marcio Habigzang Brufatto on 04/07/19.
+//  Copyright © 2019 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
